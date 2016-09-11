69e723c8294127083a6123809589e7021dba8822
Vadym Kudenchuk <vadym_kudenchuk@epam.com>
2016-09-11 10:45:59 +0300

    added some scripts and README file

---
+++ jsontoyml.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys
+import json
+import yaml
+
+# load json data
+json_data = json.loads(open(sys.argv[1]).read())
+# convert unicode to string
+converted_json_data = json.dumps(json_data)
+# output yaml
+print(yaml.dump(yaml.load(converted_json_data), default_flow_style=False))
